6092167ecca92498ce338fe2448bab02ad1f343a
Ankit <ay9698166@gmail.com>
2025-05-02 19:16:40 +0530

    a

--- models/ToastmastersRegistration.js
+++ models/ToastmastersRegistration.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ToastmastersSchema = new mongoose.Schema({
   fullName: String,
+  roll: String,
   email: String,
   phone: String,
   department: String,
